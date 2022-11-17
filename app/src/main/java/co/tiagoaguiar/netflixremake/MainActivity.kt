795b4a80681c268fb2453e99886354c02bcd846a
Luis Fortini <luisfortini12@gmail.com>
2022-11-17 14:50:04 -0300

    Initial commit

---
+++ app/src/main/java/co/tiagoaguiar/netflixremake/MainActivity.kt
@@ -0,0 +1,35 @@
+package co.tiagoaguiar.netflixremake
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import co.tiagoaguiar.netflixremake.model.Category
+import co.tiagoaguiar.netflixremake.model.Movie
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val categories = mutableListOf<Category>()
+        for(j in 0 until 10){
+
+            val movies = mutableListOf<Movie>()
+            for(i in 0 until 15){
+                val movie = Movie(R.drawable.movie)
+                movies.add(movie)
+            }
+
+            val category = Category("cat $j",movies )
+            categories.add(category)
+        }
+
+        val adapter = CategoryAdapter(categories)
+        val rv: RecyclerView = findViewById(R.id.rv_main)
+        rv.layoutManager = LinearLayoutManager(this, RecyclerView.VERTICAL, false)
+        rv.adapter = adapter
+    }
+
+}
